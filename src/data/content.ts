04f49e530dc70ab70ff6e19740b8c4171223caf1
[dyad] <git@dyad.sh>
2025-10-22 20:34:59 +0200

    Updated src/data/content.ts

--- src/data/content.ts
+++ src/data/content.ts
@@ -325,7 +325,7 @@ export const passions = [
     {
         title: "Jeux Vidéo / Esport",
         description: "Fan de jeux vidéo depuis mon plus jeune âge. Les jeux vidéo sont pour moi un moyen de me sentir mieux. Également fan d'E-sport (sport électronique) où je supporte mes équipes favorites chaque jour sur différents types de jeux. J'ai également créé un site internet dédié à ma passion pour l'E-sport dans la catégorie Portfolio.  ",
-        icon: Computer,
+        icon: Screen,
         imageUrl: "https://images.unsplash.com/photo-1550745165-9bc0b252726a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
