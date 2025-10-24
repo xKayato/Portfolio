7bed8907ab78a6cccfd970feda662c4ec1158a64
[dyad] <thomas.deloup@etu.unice.fr>
2025-10-24 17:36:10 +0200

    [dyad] Modification de la signature du développeur pour utiliser l'effet de frappe et la centrer sur le bureau en mode Windows. - wrote 1 file(s)

--- src/components/windows/DeveloperSignature.tsx
+++ src/components/windows/DeveloperSignature.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { personalInfo } from '@/data/content';
 import { cn } from '@/lib/utils';
+import { TypingEffect } from '../TypingEffect';
 
 export const DeveloperSignature = () => {
-  const [firstName, lastName] = personalInfo.name.split(' ');
+  const fullName = personalInfo.name;
 
   return (
     <div 
       className={cn(
-        "absolute bottom-12 right-4 p-2", // Au-dessus de la barre des tâches
-        "text-xs font-mono text-white/50 dark:text-white/30",
-        "pointer-events-none select-none"
+        "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2", // Centrage
+        "text-4xl md:text-6xl font-bold tracking-tight",
+        "text-white/70 dark:text-white/70", // Couleur claire pour le fond sombre du bureau
+        "pointer-events-none select-none z-0" // Assurer qu'il est derrière les icônes
       )}
     >
-      <p className="text-right">
-        {firstName} {lastName}
-      </p>
-      <p className="text-right">
-        Developer Mode
-      </p>
+      <TypingEffect 
+        text={fullName} 
+        speed={150} 
+        className="font-8bit text-shadow-lg" // Utilisation de la police 8bit si elle est définie, sinon une police stylisée
+      />
     </div>
   );
 };
